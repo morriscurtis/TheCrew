4cde3e3eadce74e8ff47175b02711b0a7dd6fa7f
Pichl0r <pichl0r@gmail.com>
2026-07-29 14:05:35 +0200

    Yielding

---
+++ MarkusCrewUnitTests/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿namespace MarkusCrewUnitTests
+{
+    public class UnitTest1
+    {
+        List<int> numbers = Enumerable.Range(1, 40).ToList();
+
+
+
+        [Fact]
+        public void YieldingTest()
+        {
+            IEnumerable<int> shuffledList = numbers.Shuffle();
+            int position = -1;
+
+            for (int i = 0; i < 4; i++)
+            {
+                var randomNumbers = shuffledList.Take(new Range(i*10, i*10+10));
+
+                if(randomNumbers.Contains(33))
+                {
+                    position = i;
+                }
+            }
+
+            Assert.True(position >= 0);
+        }
+    }
+}
